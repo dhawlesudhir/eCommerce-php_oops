84f19d76bc32da14b3a678e079c1dfda1d5e4d9b
Sudhir Dhawle <dhawlesudhir@gmail.com>
2022-11-04 18:01:40 +0530

    .env file added

--- public/index.php
+++ public/index.php
@@ -1,1 +1,6 @@
 <?php
+
+require_once __DIR__ . '/../app/config/_env.php';
+$app_name = getenv('APP_NAME');
+
+echo $app_name;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1086e9a85b42e3e92b5b18d1241d99e9dc7c0178',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1086e9a85b42e3e92b5b18d1241d99e9dc7c0178',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-ctype' => array(
+            'pretty_version' => 'v1.26.0',
+            'version' => '1.26.0.0',
+            'reference' => '6fd1b9a79f6e3cf65f9e679b23af304cd9e010d4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-ctype',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vlucas/phpdotenv' => array(
+            'pretty_version' => 'v2.6.9',
+            'version' => '2.6.9.0',
+            'reference' => '2e93cc98e2e8e869f8d9cfa61bb3a99ba4fc4141',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../vlucas/phpdotenv',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
